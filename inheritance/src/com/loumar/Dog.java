33fa398cce44141a72cf38d0a28b0eb8e55df58b
almamarie <alouismariea97@gmail.com>
2022-05-17 20:13:54 +0000

    committing to help solve do.eat() error

---
+++ inheritance/src/com/loumar/Dog.java
@@ -0,0 +1,22 @@
+package com.loumar;
+
+public class Dog extends Animal{
+
+    private int eyes;
+    private int legs;
+    private int tails;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name,int size, int weight, int eyes, int legs, int tails, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+
+        this.eyes  = eyes;
+        this.legs  = legs;
+        this.tails = tails;
+        this.teeth = teeth;
+        this.coat  = coat;
+    }
+
+
+}
---
+++ inheritance/src/com/loumar/Main.java
@@ -0,0 +1,13 @@
+package com.loumar;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+    Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+    Dog dog = new Dog("Yorkie", 12, 2, 2, 4, 1, 20,"Silky long coat");
+    dog.eat();
+}
